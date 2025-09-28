2c84464eb183a38ffc425445a01faee38fbf25eb
Debarchito Nath <debarchiton@proton.me>
2025-09-28 22:53:11 +0530

    feat(flake): remove unportable things

--- flake.nix
+++ flake.nix
@@ -1,5 +1,3 @@
-# WARN: Not portable outside NixOS based systems
-# TODO: Make it portable
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   outputs =
@@ -12,10 +10,6 @@
       devShells.${system}.default =
         (pkgs.buildFHSEnv {
           name = "harnessing_chaos";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.stdenv.cc.cc
-            "/run/opengl-driver"
-          ];
           targetPkgs = pkgs: [
             pkgs.just
             pkgs.julia
